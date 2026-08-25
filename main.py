20ab166de7b0fc957a1bac7721bfed3a2dbd00ad
Ronaldo Minero <ronaldominerov142@outlook.com>
2026-08-25 07:44:04 -0600

    boceto final

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QDialog  # Importar QDialog
-from PyQt6 import uic
 from pathlib import Path
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QWidget
 
 DIRECTORIO = Path(__file__).resolve().parent
-RUTA_UI = DIRECTORIO / "interfazmain.ui"
+RUTA_UI = DIRECTORIO / "interfazcamara.ui"
 
-class MiVentana(QDialog):  
-    def __init__(self):
-        super().__init__()
-        uic.loadUi(RUTA_UI, self)
-        
-        self.buttonBox.clicked.connect(self.al_hacer_clic)
 
-    def al_hacer_clic(self):
-        self.lblResultado.setText("¡Conectado exitosamente!")
+class MiVentana(QWidget):
+
+  def __init__(self):
+    super().__init__()
+
+    uic.loadUi(RUTA_UI, self)
+
+    self.lbl_camara.setText("Esperando señal de la cámara...")
+
+    self.veriButton.clicked.connect(self.verificar_asistencia)
+
+  def verificar_asistencia(self):
+    print("Botón presionado: Verificando...")
+    # Acción de prueba temporal
+    self.lbl_camara.setText("¡Verificación solicitada!")
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana = MiVentana()
-    ventana.show()
-    sys.exit(app.exec())
+  app = QApplication(sys.argv)
+  ventana = MiVentana()
+  ventana.show()
+  sys.exit(app.exec())
